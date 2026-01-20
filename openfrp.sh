3ca32790cf64ada72c6f45eaea23c0968c1620d9
wuzhuoyi114514 <wuzhuoyi2021404@gmail.com>
2026-01-20 21:33:46 +0800

    jq support and update support

--- openfrp.sh
+++ openfrp.sh
@@ -11,14 +11,14 @@ if [ -e .authorization ]
 then
 login=$(cat .authorization)
 echo automantic logind
-curl -X POST https://api.openfrp.net/frp/api/getUserInfo \
-         -H "Authorization: $login "
+curl -s -X POST https://api.openfrp.net/frp/api/getUserInfo \
+         -H "Authorization: $login " | jq
 else
 python oflogin.py
 login=$(cat .authorization)
 #read -s -p 'openfrp Authorization:' login
-curl -X POST https://api.openfrp.net/frp/api/getUserInfo \
-         -H "Authorization: $login "
+curl -s -X POST https://api.openfrp.net/frp/api/getUserInfo \
+         -H "Authorization: $login " | jq
 fi
 
 echo
@@ -47,7 +47,7 @@ atls=false
 proxypro=false
 fi
 echo
-curl -X POST https://api.openfrp.net/frp/api/newProxy \
+curl -s -X POST https://api.openfrp.net/frp/api/newProxy \
 -H "Authorization: a2d2a2e96dd843129723d1fba208f493YJHKMDLHNTETNJNKYI0ZMWFH" \
 -H "Content-Type: application/json" \
 -d "{
@@ -64,33 +64,33 @@ curl -X POST https://api.openfrp.net/frp/api/newProxy \
   \"autoTls\": \"$atls\",
   \"forceHttps\": false,
   \"proxyProtocolVersion\": \"$proxypro\"
-}"
+}" | jq -r .msg
 echo
 ;;
 list)
-curl -X POST "https://api.openfrp.net/frp/api/getNodeList" \
-  -H "Authorization: $login "
+curl -s -X POST "https://api.openfrp.net/frp/api/getNodeList" \
+  -H "Authorization: $login " | jq -r .data.list
 echo
 ;;
 ulist)
-curl -X POST "https://api.openfrp.net/frp/api/getUserProxies" \
-  -H "Authorization: $login "
+curl -s -X POST "https://api.openfrp.net/frp/api/getUserProxies" \
+  -H "Authorization: $login " | jq -r '.data | { total, list }'
 echo
 ;;
 remove)
 read -p "节点ID?:" id
-curl -X POST "https://api.openfrp.net/frp/api/removeProxy" \
+curl -s -X POST "https://api.openfrp.net/frp/api/removeProxy" \
   -H "Content-Type: application/json" \
   -H "Authorization: $login" \
-  -d "{\"proxy_id\": \"$id\"}"
+  -d "{\"proxy_id\": \"$id\"}" | jq .msg
 
 echo
 ;;
 login)
 python oflogin.py
 login=$(cat authorization.txt)
-curl -X POST https://api.openfrp.net/frp/api/getUserInfo \
-         -H "Authorization: $login "
+curl -s -X POST https://api.openfrp.net/frp/api/getUserInfo \
+         -H "Authorization: $login " | 
 echo
 ;;
 help) echo "add 添加节点 exit 退出 list 获取所有节点 ulist 获取用户节点 remove 删除节点 login 重新登录 start 启动节点"
@@ -98,18 +98,28 @@ help) echo "add 添加节点 exit 退出 list 获取所有节点 ulist 获取用
 "") continue
 ;;
 start)
+    latest=$(curl -s -X GET 'https://api.openfrp.net/commonQuery/get?key=software' | jq -r .data.latest_full)
 if [ -e frpc_linux_amd64 ]; then
-    continue
+    echo 进行检查更新
+    frpc_ver=$(./frpc_linux_amd64 -v)
+   if [ $frpc_ver == $latest ]
+then
+echo 版本是最新的
+else
+echo 版本不是最新的 更新中...
+   wget -O frpc.tar.gz -q https://staticassets.naids.com/client/$latest/frpc_linux_amd64.tar.gz
+    tar -xvf frpc.tar.gz
+fi
 else
     echo "你必须下载openfrp的frpc客户端才可以启动"
     echo "现在下载...."
-    wget https://staticassets.naids.com/client/OF_0.65.0_a4a4b99f_251013/frpc_linux_amd64.tar.gz
-    tar -xvf frpc_linux_amd64.tar.gz
+    wget -O frpc.tar.gz -q https://staticassets.naids.com/client/$latest/frpc_linux_amd64.tar.gz
+    tar -xvf frpc.tar.gz
 fi
  token=$(resp=$(curl -s -X POST https://api.openfrp.net/frp/api/getUserInfo \
   -H "Authorization: $(tr -d '\n\r ' < .authorization)")&&echo "$resp" | jq -r '.data.token')
 read -p "需要启动的节点ID?" startid
-./frpc_linux_amd64 -u $token -p $startid
+./frpc_linux_amd64 -u $token -p $startid -n
 
 ;;
 exit) exit
